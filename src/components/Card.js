636b235f5a48a1ce77cf5c8f69198bea806ecac4
Leonam Quintão <leonamquintao@gmail.com>
2018-11-17 20:51:02 -0200

    add style to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 export default class Card extends Component <{}> {
   render() {
     return (
-      <View></View>
+      <View style={ styles.container }>
+      </View>
     );
   }
 }
+
+const styles = new StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
+})
