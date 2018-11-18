baeebb48026979e203f064e1a60d3f7bd4c2eb6e
Leonam Quintão <leonamquintao@gmail.com>
2018-11-18 19:58:58 -0200

    add image cover to sections

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,18 +6,25 @@ import CardSection from './CardSection';
 export default class AlbumDetail extends Component<{}> {
   render() {
     /* Destruction aproach instead of use 'this.props.album' */
-    const { title, artist, thumbnail_image } = this.props.album;
+    const { title, artist, thumbnail_image, image } = this.props.album;
     return (
       <Card>
         <CardSection>
-          <View>
+          <View style={ styles.imageWrapper }>
             <Image  style={ styles.imageSpace } source={{ uri: thumbnail_image }}/>
           </View>
 
           <View style={ styles.headerContent }>
-            <Text> { title } </Text>
+            <Text style={ styles.headerTitle }> { title } </Text>
             <Text> { artist } </Text>
           </View>
+        </CardSection>
+
+        <CardSection>
+          <Image style={ styles.albumCover } source={{ uri: image }} />
+        </CardSection>
+
+        <CardSection>
         </CardSection>
       </Card>
     );
@@ -30,8 +37,23 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
   },
+  albumCover: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  imageWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 8,
+    marginRight: 10,
+  },
   headerContent: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTitle: {
+    fontSize: 16,
+    fontWeight: 'bold'
   }
 })
